bda49d4a095d957ce5d87e8e2566889621ad5d2c
Justas Palumickas <jpalumickas@gmail.com>
2019-06-05 23:04:33 +0300

    Faster check in css properties

--- src/cssNameMatch.js
+++ src/cssNameMatch.js
@@ -1,4 +1,4 @@
 import { all as cssProperties } from 'known-css-properties'
 import camelCaseCss from 'camelcase-css'
 
-export default (key) => cssProperties.find(prop => camelCaseCss(prop) === key);
+export default (key) => cssProperties.includes(camelCaseCss(key));
